3190dc125e7fa0c8c893ad9af10466f99f555a6a
Donovan Bray <donnoman@donovanbray.com>
2010-08-01 10:45:44 -0700

    Initial Rails and Bundler Config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StumppullerRails3::Application.config.secret_token = '9e0d3ba8c7e711d7f2bfa6657373e36f8409f3a303d183d2bfaff43c62e2795325dfc7e034d1cd0874ecfa35755d708afb92f2ec47c38a1bda29466a6c06d36f'
